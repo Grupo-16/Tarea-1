1c86e8ca21e30c5546fabff300334806e376b5dc
Gerssonn <gersonhernan00@hotmail.com>
2023-04-09 17:35:15 -0400

    Bug Fix: linea "package src" conflictiva

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,3 @@
-package src;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,3 @@
-package src;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
