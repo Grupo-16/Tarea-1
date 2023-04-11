622b61d9eb175d2f514076b1f31a58bf93a857b1
Gerssonn <gersonhernan00@hotmail.com>
2023-04-11 00:33:44 -0400

    Cambio nombre clase main y ejecutable para compilar

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
-public class HelloWorld {
+public class Main {
 
     
 
-    final static Logger logger = Logger.getLogger(HelloWorld.class);
+    final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
         if(args.length > 0){
