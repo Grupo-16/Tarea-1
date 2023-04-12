713aa1e1a53f5a07a4643f7fe18ef527521d2f3d
Gerssonn <gersonhernan00@hotmail.com>
2023-04-11 21:25:58 -0400

    arreglo id de prueba 2

--- src/main/java/mensajeria/Main.java
+++ src/main/java/mensajeria/Main.java
@@ -7,7 +7,7 @@ import org.apache.log4j.Logger;
 public class Main {
     final static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args){
-        if(args.length < 0){
+        if(args.length <= 0){
             System.out.println("No se han ingresado argumentos");
             return;
         }
